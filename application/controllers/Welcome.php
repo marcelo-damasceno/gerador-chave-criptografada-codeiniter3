c442724bb7deffbc3d73a7352da53f71c05726d5
marcelo-damasceno <marcelo.ad18@gmail.com>
2022-08-11 23:06:57 -0300

    commit inicial

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	public function __construct(){
+		parent::__construct();
+		$this->load->library('encryption');
+	}
+	public function index(){
+		$this->load->view('welcome_message');
+	}
+
+	public function gerar_chave(){
+		$dados['chave'] = bin2hex($this->encryption->create_key(16));
+		$dados['senha'] = 'Abc@1234';
+		$dados['senha_encriptada'] = password_hash($dados['senha'],PASSWORD_DEFAULT);
+		$this->load->view('welcome_message', $dados);
+		
+	}
+}
